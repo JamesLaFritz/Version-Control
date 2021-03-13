e17d71bc638d1bb6e9883f93c69ab25c01681a2a
James LaFritz <ktmarine1999@gmail.com>
2021-03-12 21:22:43 -0500

    Add a new script

---
+++ Assets/NewTestScript.cs
@@ -0,0 +1,15 @@
+#region Description
+// NewTestScript.cs
+// 03-12-2021
+// James LaFritz
+#endregion
+
+using UnityEngine;
+
+namespace KtMarineStudios
+{
+    public class NewTestScript : MonoBehaviour
+    {
+        
+    }
+}
