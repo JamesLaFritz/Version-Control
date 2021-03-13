83a46703c0fcb446f3b6251dd55c386d5f708925
James LaFritz <ktmarine1999@gmail.com>
2021-03-12 21:29:40 -0500

    Add a scriptable object script

---
+++ Assets/TestScriptableObject.cs
@@ -0,0 +1,16 @@
+#region Description
+// TestScriptableObject.cs
+// 03-12-2021
+// James LaFritz
+#endregion
+
+using UnityEngine;
+
+namespace KtMarineStudios
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class TestScriptableObject : ScriptableObject
+    {
+        
+    }
+}
