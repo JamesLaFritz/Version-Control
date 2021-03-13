e340f0a392d9bfac9c98571fa230ffa18f30aa85
James LaFritz <ktmarine1999@gmail.com>
2021-03-12 21:08:52 -0500

    Add a cube and a C# script

---
+++ Assets/ChallengeScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KtMarineStudios
+{
+    public class ChallengeScript : MonoBehaviour
+    {
+        /// <summary>
+        /// Start is called before the first frame update
+        /// </summary>
+        void Start()
+        {
+        
+        }
+
+        /// <summary>
+        ///  Update is called once per frame.
+        /// </summary>
+        void Update()
+        {
+        
+        }
+    }
+}
